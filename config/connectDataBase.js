13a0a79946fb4cb95ba59042c87093d164388f48
Gediwm <gedionwm@gmail.com>
2026-07-21 13:25:23 +0300

    fixing vercel bulid-logs bugs

--- config/connectDataBase.js
+++ config/connectDataBase.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 
+//I am open a global variable here
+let isConnected 
+
 const connectDB  = async()=>{
+    if(isConnected){
+        console.log('Using existing database connection')
+        return;
+    }
     try{
         const conn = await mongoose.connect(process.env.DB_String)
-        console.log(`The server is connected to the data-base ${conn.connection.host}`)
+//This prevents reconnecting the database on every request
+//when we using the app and do some things in that require DB and once we are connecting DB it doesn't require to reconnect again and again every time
+        isConnected = conn.connections[0].readyState
+        console.log(`MongoDB connected: ${conn.connection.host}`)
     }
     catch(error){
         console.log(error)
